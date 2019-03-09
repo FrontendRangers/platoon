ffe20c73df6cc9d4d5fea59e7af366d31af203a3
Benoit Deziel <ben@benoitdeziel.com>
2019-03-09 11:26:16 -0500

    chore: add storybook

--- packages/react/src/components/button/index.tsx
+++ packages/react/src/components/button/index.tsx
@@ -1,11 +1,32 @@
 import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const ButtonElement = styled('button').attrs({ type: 'button' })`
+    display: inline-block;
+`;
+
+const ButtonText = styled('div')`
+    font-size: 1rem;
+`;
+
+const ButtonIcon = styled('div')`
+    flex: 0 1 auto;
+`;
+
+const ButtonLoader = styled('div')`
+    flex: 1 0 auto;
+`;
 
 interface ButtonProps {
     children: ReactNode;
 }
 
-const Button: React.SFC<ButtonProps> = ({ children }) => (
-    <button>{children}</button>
+const Button: React.SFC<ButtonProps> = (props) => (
+    <ButtonElement {...props}>
+        <ButtonIcon />
+        <ButtonText>{props.children}</ButtonText>
+        <ButtonLoader />
+    </ButtonElement>
 );
 
 export default Button;
