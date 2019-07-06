a73bd6e2813b3f3c36792dfaab234927f824922b
Benoit Deziel <bdeziel@landr.com>
2019-07-06 12:11:22 -0400

    feat(website): update configs

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Platoon UI`,
+    description: `Accessible and themable component library for React and Vue`,
+    author: `@benoitdeziel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
