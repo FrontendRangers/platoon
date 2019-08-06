512fb35ae12144bf138b772e219b42e187975912
Benoit Deziel <bdeziel@landr.com>
2019-08-05 20:50:09 -0400

    feat(heading): update heading styles

--- packages/react/src/components/heading/Heading.tsx
+++ packages/react/src/components/heading/Heading.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import Text, { TextProps } from '../text';
+import { themeGet } from '../../themes/theme';
 
-export interface HeadingProps extends TextProps {}
+export interface HeadingProps extends TextProps {
+    as?: string;
+}
 
-const Heading = styled(Text)<HeadingProps>``;
+const Heading = styled(Text)<HeadingProps>`
+    ${({ as }) => themeGet(`textStyles[${as}]`)}
+`;
 
 Heading.defaultProps = {
     as: 'h2',
--- packages/react/src/components/heading/heading.stories.tsx
+++ packages/react/src/components/heading/heading.stories.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Heading from './';
 
-storiesOf('Components|Typography.Heading', module).add('default', () => (
-    <Heading>I'm an heading</Heading>
-));
+storiesOf('Components|Typography.Heading', module)
+    .add('default', () => <Heading>I'm an heading</Heading>)
+    .add('h1', () => <Heading as={'h1'}>I'm an heading</Heading>);
