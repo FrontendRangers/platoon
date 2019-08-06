6597e692361bbc54405687a68165e302a7539d9c
Benoit Deziel <bdeziel@landr.com>
2019-08-05 20:47:11 -0400

    feat(helpers): change function name

--- packages/react/src/themes/helpers.ts
+++ packages/react/src/themes/helpers.ts
@@ -1,7 +1,7 @@
-const getTheme = (key: string) => ({ theme }): string =>
+const themed = (key: string) => ({ theme }): string =>
     theme.components ? theme.components[key] : null;
 
 const color = (key: string) => ({ theme }): string =>
     theme.components ? theme.color[key] : null;
 
-export { getTheme, color };
+export { themed, color };
