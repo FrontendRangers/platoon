7b58a917c8cad4554c2399e633f8683a7d9e3a55
Benoit Deziel <bdeziel@landr.com>
2019-08-05 20:47:43 -0400

    feat(box): compose box styles

--- packages/react/src/primitives/box/Box.tsx
+++ packages/react/src/primitives/box/Box.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
-import { SpaceProps, space, ColorProps, color } from 'styled-system';
+import { SpaceProps, space, ColorProps, color, compose } from 'styled-system';
 
 export interface BoxProps extends SpaceProps, ColorProps {
     children?: React.ReactNode;
 }
 
 const Box = styled('div')<BoxProps>`
-    ${space};
-    ${color};
+    ${compose(
+        space,
+        color,
+    )};
 `;
 
 export default Box;
--- packages/react/src/primitives/box/box.stories.tsx
+++ packages/react/src/primitives/box/box.stories.tsx
@@ -6,6 +6,6 @@ storiesOf('Primitives|Layout.Box', module)
     .add('Default', () => <Box>Hello</Box>)
     .add('Custom padding', () => <Box p={'sm'}>I'm a box</Box>)
     .add('Custom background color', () => (
-        <Box backgroundColor={'rebeccapurple'}>I'm a box</Box>
+        <Box backgroundColor={'success'}>I'm a box</Box>
     ))
     .add('Custom text color', () => <Box color={'red'}>I'm a box</Box>);
