2aa6342a05f0afc553e097410d556b2455336658
Benoit Deziel <bdeziel@landr.com>
2019-09-07 14:40:18 -0400

    chore: update dependencies

--- packages/website/doczrc.js
+++ packages/website/doczrc.js
@@ -1,5 +1,3 @@
 export default {
-    themeConfig: {
-        mode: 'dark',
-    },
+    menu: ['Home', 'Components'],
 };
--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -8,6 +8,8 @@ module.exports = {
     plugins: [
         `gatsby-plugin-typescript`,
         `gatsby-plugin-styled-components`,
+        `gatsby-theme-docz`,
+        `gatsby-transformer-sharp`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
