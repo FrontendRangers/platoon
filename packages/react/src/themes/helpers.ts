a273010174b32218ad2d028a01d02215a1135512
Benoit Deziel <bdeziel@landr.com>
2019-10-14 09:44:20 -0400

    feat(helpers): update theme helpers

--- packages/react/src/themes/helpers.ts
+++ packages/react/src/themes/helpers.ts
@@ -1,4 +1,7 @@
-export const get = (obj: object, path: string, defaultValue: string) => {
+// HELPERS
+
+// Native get function - get value in an object depending on the path
+export const get = (obj: object, path: string, defaultValue: any) => {
     const result = String.prototype.split
         .call(path, /[,[\].]+?/)
         .filter(Boolean)
@@ -10,14 +13,31 @@ export const get = (obj: object, path: string, defaultValue: string) => {
     return result === undefined || result === obj ? defaultValue : result;
 };
 
-export const themeGet = (path: string, fallback: any = null) => (props: any) =>
-    get(props.theme, path, fallback);
+export const themeGet: any = (path: string, fallback: any = null) => ({
+    theme,
+}) => get(theme, path, fallback);
+
+export const variantGet: any = (path: string) => (props: any) => {
+    const { theme } = props;
+    const scale = path.split('.').pop();
+    const propValue = !!scale && props[scale];
+    const variant = get(theme, `${path}.${propValue}`, null);
+
+    if (!variant) {
+        return;
+    }
+
+    const variantWithMode = !!theme.mode && variant[theme.mode];
+    return variantWithMode ? variantWithMode : variant;
+};
 
 export const themed = (key: string, fallback: any = null) =>
-    themeGet(`components.${key}.styles`, fallback);
+    themeGet(`${key}`, fallback);
+
+export const variant = (key: string) => variantGet(`${key}`);
 
 export const token = (key: string, fallback: any = null) =>
-    themeGet(`tokens.${key}`, fallback);
+    themeGet(`${key}`, fallback);
 
-export const color = (key: string) => ({ theme }: any): string =>
-    theme.components ? theme.color[key] : null;
+export const color = (key: string, fallback: any = null) =>
+    themeGet(`${key}`, fallback);
