39b58bf32b815781310c086a47c7fcff77b90d42
Benoit Deziel <bdeziel@landr.com>
2020-08-10 11:01:55 -0400

    chore: update storybook and use addon-essentials

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -2,14 +2,7 @@ module.exports = {
     stories: ['../docs/**/*.stories.mdx', '../src/**/*.stories.tsx'],
     addons: [
         '@storybook/addon-toolbars',
-        '@storybook/addon-actions',
         '@storybook/addon-a11y',
-        {
-            name: '@storybook/addon-docs',
-            options: {
-                configureJSX: true,
-            },
-        },
-        '@storybook/addon-controls',
+        '@storybook/addon-essentials',
     ],
 };
