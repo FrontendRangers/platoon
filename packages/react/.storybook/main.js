9b3490830cbb8565d837b15ed4f008a2b428af84
Benoit Deziel <benoitdeziel@gmail.com>
2020-12-20 15:21:34 -0500

    chore: update storybook settings

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -2,21 +2,28 @@ module.exports = {
     stories: [
         '../src/**/*.stories.mdx',
         '../src/**/*.stories.@(js|jsx|ts|tsx)',
+        '../docs/**/*.stories.mdx',
     ],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-a11y',
+        'storybook-addon-performance/register',
     ],
-    logLevel: 'debug',
     typescript: {
+        check: true,
+        checkOptions: {},
+        reactDocgen: 'react-docgen-typescript',
         reactDocgenTypescriptOptions: {
             shouldExtractLiteralValuesFromEnum: true,
             shouldRemoveUndefinedFromOptional: true,
             propFilter: (prop) => {
-                return prop.parent
-                    ? !prop.parent.fileName.includes('react/index.d.ts')
-                    : true;
+                const isHTMLElementProp =
+                    prop.parent?.fileName.includes('react/index.d.ts') ?? false;
+                const isExternalProp =
+                    prop.parent?.fileName.includes('node_modules') ?? false;
+
+                return !(isExternalProp || isHTMLElementProp);
             },
         },
     },
